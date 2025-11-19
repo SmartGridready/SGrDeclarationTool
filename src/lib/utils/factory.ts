786f53166046b92d3b4431dbb21ac1f76b92fbe7
fatlum52 <fatlum_98@hotmail.com>
2025-11-19 16:36:59 +0100

    feat: update factory with profile identification

--- src/lib/utils/factory.ts
+++ src/lib/utils/factory.ts
@@ -7,30 +7,39 @@ export function createSampleFunctionalProfile(): FunctionalProfileFrame {
   return {
     releaseNotes: {
       state: "Draft",
-      remarks: "test remarks",
+      remarks:
+        "This is a sample functional profile demonstrating the Smart Grid Ready declaration structure. It includes comprehensive identification data and release notes for testing and development purposes.",
       changeLog: [
         {
           version: "1.0.0",
           date: "2024-01-15",
-          author: "System",
-          comment: "Initial release",
+          author: "Development Team",
+          comment:
+            "Initial release - Sample functional profile created for testing",
         },
         {
           version: "0.9.0",
           date: "2024-01-14",
-          author: "Developer",
-          comment: "Beta version",
+          author: "QA Team",
+          comment: "Beta version - Pre-release testing and validation",
+        },
+        {
+          version: "0.8.0",
+          date: "2024-01-08",
+          author: "Development Team",
+          comment:
+            "Alpha version - Initial implementation and structure definition",
         },
       ],
     },
     functionalProfile: {
       functionalProfileIdentification: {
-        specificationOwnerIdentification: "Test",
+        specificationOwnerIdentification: "SGR-Example-2024",
         functionalProfileCategory: "Battery",
-        functionalProfileType: "",
+        functionalProfileType: "Lithium-Ion Battery System",
         levelOfOperation: "1",
         versionNumber: {
-          primaryVersionNumber: 0,
+          primaryVersionNumber: 1,
           secondaryVersionNumber: 0,
           subReleaseVersionNumber: 0,
         },
@@ -46,7 +55,7 @@ export function createEmpty(): FunctionalProfileFrame {
   return {
     functionalProfile: {
       functionalProfileIdentification: {
-        specificationOwnerIdentification: "Test",
+        specificationOwnerIdentification: "",
         functionalProfileCategory: "Battery",
         functionalProfileType: "",
         levelOfOperation: "1",

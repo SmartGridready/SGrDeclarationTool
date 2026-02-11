3f840c15daa8ee4c9635db68d9ee0b2e7a1fc871
Matthias Krebs <matthias.krebs@ateleris.ch>
2026-02-11 11:24:59 +0100

    feat: display version in navigation bar.

--- src/components/menu/navbar.tsx
+++ src/components/menu/navbar.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 import { Button } from "@/components/shadcn/button";
 import { Logo } from "@/components/menu/logo";
 import { ThemeToggle } from "@/components/theme/theme-toggle";
+import pack from "../../../package.json";
 
 export function Navbar() {
   return (
     <nav className="border-b bg-background">
       <div className="flex h-16 items-center px-8">
-        <Logo />
+        <div>
+          <Logo />
+          <span className="text-xs leading-none">{pack.version}</span>
+        </div>
         <div className="ml-auto flex items-center gap-4">
           <Button variant="ghost" asChild>
             <Link href="/devices">Devices</Link>
